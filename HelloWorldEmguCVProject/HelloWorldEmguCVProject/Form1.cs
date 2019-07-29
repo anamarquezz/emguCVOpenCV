9090c055a49c363e73afcba64df7069c8e7ff527
AnaMarquez <anamarquez.edu@hotmail.com>
2019-07-29 16:43:05 -0700

    Hello world

---
+++ HelloWorldEmguCVProject/HelloWorldEmguCVProject/Form1.cs
@@ -0,0 +1,50 @@
+﻿using Emgu.CV;
+using Emgu.CV.Structure;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+
+
+namespace HelloWorldEmguCVProject
+{
+    
+    public partial class Form1 : Form
+    {
+        //The name of the window
+        String win1 = "Test Window";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Create the window using the specific name
+            CvInvoke.cvNamedWindow(win1);
+
+            //Create an image of 400x200 of Blue colo`r
+            using (Image<Bgr, Byte> img = new Image<Bgr, byte>(400, 200, new Bgr(255, 0, 0)))
+            {
+                //Create the font
+                MCvFont f = new MCvFont(Emgu.CV.CvEnum.FONT.CV_FONT_HERSHEY_COMPLEX, 1.0, 1.0);
+                //Draw "Hello, world." on the image using the specific font
+                img.Draw("Hello, world", ref f, new Point(10, 80), new Bgr(0, 255, 0));
+
+                //Show the image
+                CvInvoke.cvShowImage(win1, img.Ptr);
+                //Wait for the key pressing event
+                CvInvoke.cvWaitKey(0);
+                //Destory the window
+                CvInvoke.cvDestroyWindow(win1);
+            }
+        }
+    }
+}
+
